8e4854e782a3cdc8012355bc8825ceadc753b07b
Ademar Arvati Filho <arvati@hotmail.com>
2022-08-14 19:02:06 -0300

    howto hardhat site

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,7 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -45,6 +46,13 @@ contract Token {
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        console.log(
+            "Transferring from %s to %s %s tokens",
+            msg.sender,
+            to,
+            amount
+        );
 
         // Transfer the amount.
         balances[msg.sender] -= amount;
