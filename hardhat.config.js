0fb4813a3718b9aa3626b53f846112b2497c23cc
Ademar Arvati Filho <arvati@hotmail.com>
2022-08-14 22:09:24 -0300

    contrato com deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,14 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-ethers")
 
 require('dotenv').config();
 
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const INFURA_KEY = process.env.INFURA_KEY;
 
 module.exports = {
@@ -13,8 +18,22 @@ module.exports = {
       url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
       accounts: [`0x${WALLET_PRIVATE_KEY}`]
     },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
+    }
   },
   etherscan: {
-    apiKey: POLYGONSCAN_API_KEY
-  },
+    apiKey: {
+        //ethereum
+        mainnet: ETHERSCAN_API_KEY,
+        ropsten: ETHERSCAN_API_KEY,
+        rinkeby: ETHERSCAN_API_KEY,
+        goerli: ETHERSCAN_API_KEY,
+        kovan: ETHERSCAN_API_KEY,
+        //polygon
+        polygon: POLYGONSCAN_API_KEY,
+        polygonMumbai: POLYGONSCAN_API_KEY
+   }
+}
 };
---
+++ scripts/annotation.js
@@ -0,0 +1,32 @@
+/*
+Mumbai Deploy
+Deploying contracts with the account:, 0xe37DF09A14CCC1fDB3dffeD559bc059A21b8C13D
+Account balance:, 999522240080587203
+Token address Tarefa 01:, 0xbfd260ddF1936d3ac0ded8E6D52B9f6E8EC70153
+Token address Tarefa 02:, 0x6182A9cf01D0CE53F8be3d210128e565E9DE660B
+Token address Tarefa 03:, 0x40d72AaE3adcFD55742b3f2a3E35eAb27bB979aE
+Token address Tarefa 04:, 0xF96a046F1E5E32A35Bd82F1EEad704752ce19E96
+
+https://mumbai.polygonscan.com/address/0xbfd260ddF1936d3ac0ded8E6D52B9f6E8EC70153
+https://mumbai.polygonscan.com/address/0x6182A9cf01D0CE53F8be3d210128e565E9DE660B
+https://mumbai.polygonscan.com/address/0x40d72AaE3adcFD55742b3f2a3E35eAb27bB979aE
+https://mumbai.polygonscan.com/address/0xF96a046F1E5E32A35Bd82F1EEad704752ce19E96
+
+
+npx hardhat verify --network mumbai 0xbfd260ddF1936d3ac0ded8E6D52B9f6E8EC70153 --contract contracts/Tarefa01.sol:Tarefa01
+
+https://mumbai.polygonscan.com/address/0xbfd260ddF1936d3ac0ded8E6D52B9f6E8EC70153#code
+
+npx hardhat verify --network mumbai 0x6182A9cf01D0CE53F8be3d210128e565E9DE660B --contract contracts/Tarefa02.sol:Tarefa02
+
+https://mumbai.polygonscan.com/address/0x6182A9cf01D0CE53F8be3d210128e565E9DE660B#code
+
+npx hardhat verify --network mumbai 0x40d72AaE3adcFD55742b3f2a3E35eAb27bB979aE --contract contracts/Tarefa03.sol:Tarefa03
+
+https://mumbai.polygonscan.com/address/0x40d72AaE3adcFD55742b3f2a3E35eAb27bB979aE#code
+
+npx hardhat verify --network mumbai 0xF96a046F1E5E32A35Bd82F1EEad704752ce19E96 --contract contracts/Tarefa04.sol:Tarefa04
+
+https://mumbai.polygonscan.com/address/0xF96a046F1E5E32A35Bd82F1EEad704752ce19E96#code
+
+/*
