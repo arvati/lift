21200d35d57e21877e5a44d0641e6eebd3867ebc
Ademar Arvati Filho <arvati@hotmail.com>
2022-08-14 21:19:48 -0300

    Tarefa aula

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const INFURA_KEY = process.env.INFURA_KEY;
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    matic: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,14 +3,30 @@ const { ethers } = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log(`Deploying contracts with the account:, ${deployer.address}`);
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log(`Account balance:, ${(await deployer.getBalance()).toString()}`);
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
+  const Token1 = await ethers.getContractFactory("Tarefa01");
+  const token1 = await Token1.deploy();
 
-  console.log("Token address:", token.address);
+  const Token2 = await ethers.getContractFactory("Tarefa02");
+  const token2 = await Token2.deploy();
+
+  const Token3 = await ethers.getContractFactory("Tarefa03");
+  const token3 = await Token3.deploy();
+
+  const Token4 = await ethers.getContractFactory("Tarefa04");
+  const token4 = await Token4.deploy();
+
+  //const Token5 = await ethers.getContractFactory("SellerTarefa04");
+  //const token5 = await Token5.deploy();
+
+  console.log(`Token address Tarefa 01:, ${token1.address}`);
+  console.log(`Token address Tarefa 02:, ${token2.address}`);
+  console.log(`Token address Tarefa 03:, ${token3.address}`);
+  console.log(`Token address Tarefa 04:, ${token4.address}`);
+  //console.log(`Token address Seller Tarefa 04:, ${token5.address}`);
 }
 
 main()
