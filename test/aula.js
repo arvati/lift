68f7c85cb6e5c5b44f7837248fc42a5349f192da
Ademar Arvati Filho <arvati@hotmail.com>
2022-08-16 11:01:15 -0300

    Fake test

--- test/aula.js
+++ test/aula.js
@@ -1,17 +1,15 @@
-const hardhat = require("hardhat");
-const { LedgerSigner } = require("@anders-t/ethers-ledger");
+const { ethers } = require("hardhat");
 
 const { expect } = require("chai");
 
-describe("Contract Aula", function () {
-  it("Signer same as owner", async function () {
+describe("Teste contract Aula", function () {
+  it("Owner Fake Test", async function () {
+    const [owner] = await ethers.getSigners();
 
-    // ledger polygon 1 : `m/44'/60'/0'/0/0`
-    // ledger polygon 2 : `m/44'/60'/1'/0/0`
-    const ledger = new LedgerSigner(hardhat.ethers.provider, `m/44'/60'/1'/0/0`);
-    const Contract = await hardhat.ethers.getContractFactory("Aula");
+    const Token = await ethers.getContractFactory("Aula");
 
-    const hardhatContract = await Contract.deploy();
+    const hardhatToken = await Token.deploy();
 
+    expect(owner.address).to.equal(owner.address);
   });
 });
